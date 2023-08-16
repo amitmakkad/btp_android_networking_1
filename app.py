24e9c95ed0de046b4f1ea396aab9d2edb3ff0fc7
Amit Kumar Makkad <79632719+amitmakkad@users.noreply.github.com>
2023-08-16 18:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Web App with Python Flask'
+
 @app.route('/get_base_url', methods=['GET'])
 def get_base_url():
     base_url = "https://21ce-103-159-214-186.ngrok-free.app"  # Replace with your server's base URL
