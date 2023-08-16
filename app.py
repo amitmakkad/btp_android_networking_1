a14f1098010ce0c2adee058d421da048649a002a
Amit Kumar Makkad <79632719+amitmakkad@users.noreply.github.com>
2023-08-16 18:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/get_base_url', methods=['GET'])
 def get_base_url():
-    base_url = "https://b031-103-159-214-223.ngrok-free.app/"  # Replace with your server's base URL
+    base_url = "https://21ce-103-159-214-186.ngrok-free.app"  # Replace with your server's base URL
     return jsonify({"baseUrl": base_url})
 
 if __name__ == '__main__':
